57238feaf5d6260904aba2164e874cca7cdb8c48
R2\ivanchenko.oleksii <oleksii.ivanchenko@actum.cz>
2017-02-04 16:31:33 +0100

    Added domain project

---
+++ XoredTest.Chat/XoredTest.Chat.Domain/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XoredTest.Chat.Domain
+{
+    /// <summary>
+    /// User entity
+    /// </summary>
+    public class User
+    {
+        public User(string id, string name, UserType userType)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.UserType = userType;
+        }
+
+        public string Id { get; private set; }
+
+        public string Name { get; private set; }
+
+        public UserType UserType { get; private set; }
+    }
+
+}
---
+++ XoredTest.Chat/XoredTest.Chat.Domain/UserType.cs
@@ -0,0 +1,11 @@
+﻿namespace XoredTest.Chat.Domain
+{
+    /// <summary>
+    /// Types of user
+    /// </summary>
+    public enum UserType
+    {
+        Anonymous,
+        General
+    }
+}
