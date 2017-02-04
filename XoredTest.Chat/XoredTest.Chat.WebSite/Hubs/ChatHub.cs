ee7c7044a97dc09da75bed110e549332ed3951b1
R2\ivanchenko.oleksii <oleksii.ivanchenko@actum.cz>
2017-02-04 16:32:21 +0100

    Added functionality with anonymus users and notification about connections.

--- XoredTest.Chat/XoredTest.Chat.WebSite/Hubs/ChatHub.cs
+++ XoredTest.Chat/XoredTest.Chat.WebSite/Hubs/ChatHub.cs
@@ -1,28 +1,71 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNet.SignalR;
+using XoredTest.Chat.Domain;
 
 namespace XoredTest.Chat.WebSite.Hubs
 {
+    /// <summary>
+    /// Hub for proccessing chat
+    /// </summary>
     public class ChatHub : Hub
     {
+        private static List<User> connectedUsers = new List<User>();
+
+        /// <summary>
+        /// Sends messages to all user in public room
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <param name="msg"></param>
         public void SendToAll(string userName, string msg)
         {
-            if(string.IsNullOrWhiteSpace(msg))
+            if (string.IsNullOrWhiteSpace(msg))
                 return;
-
-            if (string.IsNullOrWhiteSpace(userName))
-            {
-                userName = "Anonimus";
-            }
-
-
+            
             this.Clients.All.broadcastMessage(userName, msg);
         }
 
+        /// <summary>
+        /// Register new user
+        /// </summary>
+        /// <param name="name"></param>
+        public void Connect(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                name = this.AddAnonymusUser();
+                this.Clients.Caller.setNewName(name);
+            }
+            else
+            {
+                connectedUsers.Add(new User(this.Context.ConnectionId, name, UserType.General));
+            }
+            this.Clients.AllExcept(this.Context.ConnectionId).notifyAboutNewUser(name);
+        }
+
+        /// <summary>
+        /// Add anonymus user
+        /// </summary>
+        /// <returns></returns>
+        private string AddAnonymusUser()
+        {
+            var anonCount = connectedUsers.Count(t => t.UserType == UserType.Anonymous);
+
+            var userName = $"Anonimus{anonCount}";
+            connectedUsers.Add(new User(this.Context.ConnectionId, userName, UserType.Anonymous ));
+            return userName;
+        }
+
+        /// <summary>
+        /// Sends message to user by id
+        /// </summary>
+        /// <param name="userId">Reciever id</param>
+        /// <param name="msg">Message text</param>
         public void SendPrivateTo(string userId, string msg)
         {
             throw new NotImplementedException();
         }
-        
+
     }
 }
--- XoredTest.Chat/XoredTest.Chat.WebSite/Scripts/main.js
+++ XoredTest.Chat/XoredTest.Chat.WebSite/Scripts/main.js
@@ -1,27 +1,51 @@
-﻿
+﻿// Declare a proxy to reference the hub.
+var chat = $.connection.chatHub;
+
+function broadcastMsg(name, message) {
+    // Html encode display name and message.
+    var encodedName = $('<div />').text(name).html();
+    var encodedMsg = $('<div />').text(message).html();
+    // Add the message to the page.
+    $('#discussion').append('<li><strong>' + encodedName
+        + '</strong>:&nbsp;&nbsp;' + encodedMsg + '</li>');
+};
+
+function setNewName(name) {
+    $('#displayname').val(name);
+}
+
+function sendMsgToAll() {
+    $('#sendmessage').click(function() {
+        // Call the Send method on the hub.
+        chat.server.sendToAll($('#displayname').val(), $('#message').val());
+        // Clear text box and reset focus for next comment.
+        $('#message').val('').focus();
+    });
+};
+
+function ConnectUser(userName) {
+    chat.server.connect($('#displayname').val());
+};
+
+function notifyAboutNewUser(name) {
+    var encodedMsg = $('<div />').text(name).html();
+    // Add the message to the page.
+    $('#discussion').append('<li>New user in chat with name:&nbsp;&nbsp;' + encodedMsg + '</li>');
+};
+
 $(function init() {
-    // Declare a proxy to reference the hub.
-    var chat = $.connection.chatHub;
     // Create a function that the hub can call to broadcast messages.
-    chat.client.broadcastMessage = function (name, message) {
-        // Html encode display name and message.
-        var encodedName = $('<div />').text(name).html();
-        var encodedMsg = $('<div />').text(message).html();
-        // Add the message to the page.
-        $('#discussion').append('<li><strong>' + encodedName
-            + '</strong>:&nbsp;&nbsp;' + encodedMsg + '</li>');
-    };
+    chat.client.broadcastMessage = broadcastMsg;
+    chat.client.notifyAboutNewUser = notifyAboutNewUser;
+    chat.client.setNewName = setNewName;
     // Get the user name and store it to prepend to messages.
     $('#displayname').val(prompt(general.entrName, ''));
     // Set initial focus to message input box.
     $('#message').focus();
     // Start the connection.
-    $.connection.hub.start().done(function () {
-        $('#sendmessage').click(function () {
-            // Call the Send method on the hub.
-            chat.server.sendToAll($('#displayname').val(), $('#message').val());
-            // Clear text box and reset focus for next comment.
-            $('#message').val('').focus();
-        });
-    });
+    $.connection.hub.start().done(ConnectUser);
+
+    sendMsgToAll();
+    var notify = notifyAboutNewUser;
+
 });
